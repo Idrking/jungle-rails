2b22da7989b3d6ee7176a401c17f17f2d3a52b09
Isaac King <idrkings@gmail.com>
2021-03-09 22:29:44 +0000

    Add features spec for product detail page

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
+require_relative 'images'
 
-RSpec.feature "ProductDetails", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "Visitor navigates to product detail page", type: :feature, js: true do
+  
+  before :each do
+    @category = Category.create! name: "Apparel"
+
+    10.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        quantity: 10,
+        price: 64.99,
+        image: open_asset('apparel1.jpg')
+      )
+    end
+  end
+  
+  scenario "From the home page" do
+  visit root_path
+
+  first('article.product').find_link('Details').click()
+  
+  expect(page).to have_css 'article.product-detail'
+
+  end
 end
